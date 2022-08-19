03b7e40246f6a9581b043402741932640ae6a224
Illia Diachenko <ilya.dyachenko@gmail.com>
2022-08-19 23:50:54 +0300

    backup

--- 2.3-scan.js
+++ 2.3-scan.js
@@ -8,10 +8,6 @@ const ddb = new AWS.DynamoDB({
 async function getItem() {
   const params = {
     TableName: "Movies",
-    // FilterExpression: "titleType = :titleType",
-    // ExpressionAttributeValues: {
-    //   ":titleType": { S: "movie" },
-    // },
   };
 
   try {
@@ -23,3 +19,38 @@ async function getItem() {
 }
 
 getItem();
+
+// 1: Get count of items in table
+// const params = {
+//   TableName: "Movies",
+// };
+
+// 2: Get item by hash key
+// const params = {
+//   TableName: "Movies",
+//   FilterExpression: "tconst = :tconst",
+//   ExpressionAttributeValues: {
+//     ":tconst": { S: "tt12374656" },
+//   },
+// };
+
+// 3: Filter items by attribute value
+// const params = {
+//   TableName: "Movies",
+//   FilterExpression: "isAdult = :isAdult",
+//   ExpressionAttributeValues: {
+//     ":isAdult": { BOOL: true },
+//   },
+// };
+
+// SLIDE: Conditions
+// 4: Filter by contains
+// const params = {
+//   TableName: "Movies",
+//   FilterExpression: "contains(genres, :genres)",
+//   ExpressionAttributeValues: {
+//     ":genres": { S: "Drama" },
+//   },
+// };
+
+// 5: Delete items without rating
