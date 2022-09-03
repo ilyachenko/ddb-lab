1c07c63eecdda001a6349477ee905c93a2dd1c28
Illia Diachenko <ilya.dyachenko@gmail.com>
2022-09-03 20:36:09 +0300

    2.1

--- 2.1-get-item.js
+++ 2.1-get-item.js
@@ -1,13 +1,20 @@
+import AWS from "aws-sdk";
 import { ddb } from "./helpers/index.js";
 
 async function getItem() {
   const params = {
     TableName: "Movies",
+    Key: {
+      tconst: { S: "tt7286456" },
+      sk: { S: "MOVIE" },
+    },
   };
 
   try {
     const results = await ddb.getItem(params).promise();
     console.log(results.Item);
+    const unmarshaled = AWS.DynamoDB.Converter.unmarshall(results.Item);
+    console.log(unmarshaled);
   } catch (error) {
     console.log(error);
   }
