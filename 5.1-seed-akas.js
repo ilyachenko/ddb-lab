b23c83f59fbc7371141590a7d1473f50b9f83123
Illia Diachenko <ilya.dyachenko@gmail.com>
2022-09-03 21:12:53 +0300

    ..

--- 5.1-seed-akas.js
+++ 5.1-seed-akas.js
@@ -27,54 +27,54 @@ function seed() {
     const { titleId, ordering, title, region, language, types } =
       lineParser.parse(line);
 
-    if (types !== "imdbDisplay") {
-      return;
-    }
+    // if (types !== "imdbDisplay") {
+    //   return;
+    // }
 
-    const params = {
-      TableName: "Movies",
-      Item: {
-        tconst: {
-          S: titleId,
-        },
-        type: {
-          S: "akas",
-        },
-        sk: {
-          S: `#AKAS#${titleId}#${ordering}`,
-        },
-        title: {
-          S: title,
-        },
-        region: {
-          S: region,
-        },
-        language: {
-          S: language,
-        },
-      },
-      ReturnConsumedCapacity: "TOTAL",
-    };
+    // const params = {
+    //   TableName: "Movies",
+    //   Item: {
+    //     tconst: {
+    //       S: titleId,
+    //     },
+    //     type: {
+    //       S: "akas",
+    //     },
+    //     sk: {
+    //       S: `#AKAS#${titleId}#${ordering}`,
+    //     },
+    //     title: {
+    //       S: title,
+    //     },
+    //     region: {
+    //       S: region,
+    //     },
+    //     language: {
+    //       S: language,
+    //     },
+    //   },
+    //   ReturnConsumedCapacity: "TOTAL",
+    // };
 
-    if (region === "\\N") {
-      delete params.Item.region;
-    }
+    // if (region === "\\N") {
+    //   delete params.Item.region;
+    // }
 
-    if (language === "\\N") {
-      delete params.Item.language;
-    }
+    // if (language === "\\N") {
+    //   delete params.Item.language;
+    // }
 
-    try {
-      log(counter, seedCounter);
-      await ddb
-        .putItem(params)
-        .promise()
-        .then(() => log(counter, ++seedCounter));
-    } catch (error) {
-      console.log(error);
-      console.log(params);
-      process.exit(1);
-    }
+    // try {
+    //   log(counter, seedCounter);
+    //   await ddb
+    //     .putItem(params)
+    //     .promise()
+    //     .then(() => log(counter, ++seedCounter));
+    // } catch (error) {
+    //   console.log(error);
+    //   console.log(params);
+    //   process.exit(1);
+    // }
   });
 }
 
