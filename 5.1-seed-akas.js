daa600324bd3fdd148af808bf85ad28ee41e9716
Illia Diachenko <ilya.dyachenko@gmail.com>
2022-09-03 21:21:16 +0300

    ..

--- 5.1-seed-akas.js
+++ 5.1-seed-akas.js
@@ -1,24 +1,12 @@
-// System libs
-import fs from "fs";
-import path from "path";
-import readline from "readline";
-
-// Helpers
-import { ddb, currDir, log, LineParser } from "./helpers/index.js";
+import { ddb, log, LineParser, LineReader } from "./helpers/index.js";
 
 const lineParser = new LineParser();
 
 function seed() {
-  const filePath = path.join(currDir(import.meta.url) + "/data/title.akas.tsv");
-
   let counter = 0;
   let seedCounter = 0;
 
-  var lineReader = readline.createInterface({
-    input: fs.createReadStream(filePath),
-  });
-
-  lineReader.on("line", async (line) => {
+  LineReader("title.akas", async (line) => {
     if (counter++ === 0) {
       lineParser.setColumn(line);
       return;
@@ -27,54 +15,54 @@ function seed() {
     const { titleId, ordering, title, region, language, types } =
       lineParser.parse(line);
 
-    // if (types !== "imdbDisplay") {
-    //   return;
-    // }
+    if (types !== "imdbDisplay") {
+      return;
+    }
 
-    // const params = {
-    //   TableName: "Movies",
-    //   Item: {
-    //     tconst: {
-    //       S: titleId,
-    //     },
-    //     type: {
-    //       S: "akas",
-    //     },
-    //     sk: {
-    //       S: `#AKAS#${titleId}#${ordering}`,
-    //     },
-    //     title: {
-    //       S: title,
-    //     },
-    //     region: {
-    //       S: region,
-    //     },
-    //     language: {
-    //       S: language,
-    //     },
-    //   },
-    //   ReturnConsumedCapacity: "TOTAL",
-    // };
+    const params = {
+      TableName: "Movies",
+      Item: {
+        tconst: {
+          S: titleId,
+        },
+        type: {
+          S: "akas",
+        },
+        sk: {
+          S: `AKAS#${titleId}#${ordering}`,
+        },
+        title: {
+          S: title,
+        },
+        region: {
+          S: region,
+        },
+        language: {
+          S: language,
+        },
+      },
+      ReturnConsumedCapacity: "TOTAL",
+    };
 
-    // if (region === "\\N") {
-    //   delete params.Item.region;
-    // }
+    if (region === "\\N") {
+      delete params.Item.region;
+    }
 
-    // if (language === "\\N") {
-    //   delete params.Item.language;
-    // }
+    if (language === "\\N") {
+      delete params.Item.language;
+    }
 
-    // try {
-    //   log(counter, seedCounter);
-    //   await ddb
-    //     .putItem(params)
-    //     .promise()
-    //     .then(() => log(counter, ++seedCounter));
-    // } catch (error) {
-    //   console.log(error);
-    //   console.log(params);
-    //   process.exit(1);
-    // }
+    try {
+      log(counter, seedCounter);
+      await ddb
+        .putItem(params)
+        .promise()
+        .then(() => log(counter, ++seedCounter));
+    } catch (error) {
+      console.log(error);
+      console.log(params);
+      process.exit(1);
+    }
   });
 }
 
